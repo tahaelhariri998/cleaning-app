615cd2228a73521ff03f8b2a7da61924c637962b
Taha elhariri <tahaalhriri@gmail.com>
2025-01-02 22:33:48 +0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 // pages/_app.tsx
 import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
