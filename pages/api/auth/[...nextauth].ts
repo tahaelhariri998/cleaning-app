a950d6e780b761bc6e5e3e98562b455339871e68
TAHA ELHARİRİ <73181409+tahaelhariri998@users.noreply.github.com>
2025-01-03 19:08:19 +0300

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          redirect_uri: 'https://cleaning-app-sand.vercel.app/api/auth/callback/google', // Vercel redirect URI
+          redirect_uri: 'https://cleaning-app-sand.vercel.app', // Vercel redirect URI
         }
       }
     }),
