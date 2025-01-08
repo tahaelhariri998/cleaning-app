cfd71d26d92bac3fb1de0e23b1a5891a74ea0d24
Taha elhariri <tahaalhriri@gmail.com>
2025-01-08 17:08:10 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,38 +3,10 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'https-calls',
-        networkTimeoutSeconds: 15,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60
-        },
-        cacheableResponse: {
-          statuses: [0, 200]
-        }
-      }
-    },
-    {
-      // Cache NextAuth.js session endpoint
-      urlPattern: /\/api\/auth\/session/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'auth-session',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 24 * 60 * 60 // 24 hours
-        }
-      }
-    }
-  ]
 })
 
 module.exports = withPWA({
-  reactStrictMode: false
+  // next.js config
+  reactStrictMode: false,
+  swcMinify: false,
 })
