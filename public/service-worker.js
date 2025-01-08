c1a538152b2c0adc1414ef267819f0224fd9f799
Taha elhariri <tahaalhriri@gmail.com>
2025-01-08 15:05:25 +0300

    Initial commit

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  // Add other routes you want to cache
+  
+  '/profile',
+  // Add your static assets
+  '/logo.png',
+  '/styles/main.css',
+ " /globals.css",
+ "/ProfileRating.tsx",
+ "/_app.tsx",
+
+
+
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version if found
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it can only be used once
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because it can only be used once
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
