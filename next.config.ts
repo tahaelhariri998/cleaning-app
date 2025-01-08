49e6dcca9fbed743257494177e1b81f1e4f3b88d
Taha elhariri <tahaalhriri@gmail.com>
2025-01-08 14:39:45 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,36 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+  ],
 })
 
 module.exports = withPWA({
-  // next.js config
   reactStrictMode: false,
-  swcMinify: false,
+  // Removed swcMinify as it's default now
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/'
+          }
+        ]
+      }
+    ]
+  }
 })
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 // pages/_app.tsx
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useEffect } from 'react';
+import { registerServiceWorker } from '../utils/register-sw';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    registerServiceWorker();
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
